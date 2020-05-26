86c429042fdcda140ad74f6a0838fb87c9457e72
carloslima78 <carlos.lima78@gmail.com>
2020-05-26 13:07:39 -0300

    Ajustes na UML e código referente ao SRP.

--- Principios.SOLID/01. SRP/Solucao/Cliente.cs
+++ Principios.SOLID/01. SRP/Solucao/Cliente.cs
@@ -6,17 +6,6 @@ namespace Itau.SRP.Solucao
         public int IdCliente { get; set; }
         public string Nome { get; set; }
         public string CPF { get; set; }
-        public string Email { get; set; }
-
-        public void Inserir (Cliente cliente)
-        {
-            RepositorioCliente repositorio = new RepositorioCliente();
-            cliente.IdCliente = repositorio.Inserir(cliente);
-
-            if(cliente.IdCliente > 0)
-            {
-                ServicoEmail.Enviar("Saudações", "Saudações ao novo cliente", "ben@contoso", cliente.Email);
-            }
-        }
+        public string Email { get; set; }  
     }
 }
---
+++ Principios.SOLID/01. SRP/Solucao/ServicoCliente.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Itau.SRP.Solucao
+{
+    public class ServicoCliente
+    {
+        public void Inserir(Cliente cliente)
+        {
+            RepositorioCliente repositorio = new RepositorioCliente();
+            cliente.IdCliente = repositorio.Inserir(cliente);
+
+            if (cliente.IdCliente > 0)
+            {
+                ServicoEmail.Enviar("Saudações", "Saudações ao novo cliente", "ben@contoso", cliente.Email);
+            }
+        }
+    }
+}
